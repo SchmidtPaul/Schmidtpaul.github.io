5194d5ca5a45340c1f205ddae0a9c49a1294ffb4
SchmidtPaul <schmidtpaul@hotmail.de>
2023-08-22 20:52:51 +0200

    Update Workshops

--- docs/src/plot workshop duration.R
+++ docs/src/plot workshop duration.R
@@ -1,4 +1,3 @@
-library(ggrepel)
 library(here)
 library(readxl)
 library(tidyverse)
@@ -13,20 +12,65 @@ dat <- read_excel(here("src", "workshops.xlsx")) %>%
 pdat <- dat %>% 
   group_by(year) %>% 
   summarise(
-    sum = sum(dur)
+    sum = sum(dur),
+    n = n()
   ) %>%
   ungroup()
 
 ggplot(data = pdat) +
   aes(y = sum, x = year) +
   geom_line(size = 1) +
+  geom_text(
+    aes(label = sum),
+    hjust = 1,
+    vjust = 0,
+    position = position_nudge(y = 5)
+  ) +
+  geom_text(
+    aes(label = n),
+    hjust = 0,
+    vjust = 1,
+    position = position_nudge(y = -5)
+  ) +
   scale_y_continuous(
     name = "Total hours of workshops",
     limits = c(0, NA),
     breaks = scales::breaks_width(50),
     expand = expansion(mult = c(0, 0.05))
   ) +
-  scale_x_continuous(
-    name = NULL
-  ) +
+  scale_x_continuous(name = NULL) +
   theme_minimal()
+
+library(ggplot2)
+
+ggplot(data = pdat) +
+  aes(y = sum, x = year) +
+  geom_area(fill = "grey80", alpha = 0.5) +
+  geom_line(size = 1) +
+  geom_text(
+    aes(label = sum)
+  ) +
+  scale_y_continuous(
+    name = "Total hours of workshops",
+    limits = c(0, NA),
+    breaks = scales::breaks_width(50),
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  scale_x_continuous(name = NULL) +
+  theme_minimal() 
+  
+
+
+
+library(tidyverse)
+
+dat <- tibble(x = c(1, 2, 3, 4),
+              y = c(1, 7, 5, 9))
+
+ggplot(dat) +
+  aes(x = x, y = y, label = y) +
+  geom_line() +
+  geom_label(color = NA) +
+  geom_text() +
+  theme_classic()
+
--- src/plot workshop duration.R
+++ src/plot workshop duration.R
@@ -21,12 +21,16 @@ ggplot(data = pdat) +
   aes(y = sum, x = year) +
   geom_line(size = 1) +
   geom_text(
-    aes(label = sum), 
+    aes(label = sum),
     hjust = 1,
     vjust = 0,
-    position = position_nudge(
-      x = -0.05, y = 5
-    )
+    position = position_nudge(y = 5)
+  ) +
+  geom_text(
+    aes(label = n),
+    hjust = 0,
+    vjust = 1,
+    position = position_nudge(y = -5)
   ) +
   scale_y_continuous(
     name = "Total hours of workshops",
@@ -34,7 +38,39 @@ ggplot(data = pdat) +
     breaks = scales::breaks_width(50),
     expand = expansion(mult = c(0, 0.05))
   ) +
-  scale_x_continuous(
-    name = NULL
-  ) +
+  scale_x_continuous(name = NULL) +
   theme_minimal()
+
+library(ggplot2)
+
+ggplot(data = pdat) +
+  aes(y = sum, x = year) +
+  geom_area(fill = "grey80", alpha = 0.5) +
+  geom_line(size = 1) +
+  geom_text(
+    aes(label = sum)
+  ) +
+  scale_y_continuous(
+    name = "Total hours of workshops",
+    limits = c(0, NA),
+    breaks = scales::breaks_width(50),
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  scale_x_continuous(name = NULL) +
+  theme_minimal() 
+  
+
+
+
+library(tidyverse)
+
+dat <- tibble(x = c(1, 2, 3, 4),
+              y = c(1, 7, 5, 9))
+
+ggplot(dat) +
+  aes(x = x, y = y, label = y) +
+  geom_line() +
+  geom_label(color = NA) +
+  geom_text() +
+  theme_classic()
+
